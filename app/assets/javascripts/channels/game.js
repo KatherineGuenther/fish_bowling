de10a45a6a8d9e905b39182bb5231854bad914b7
Katherine Guenther <guenther@peak.org>
2017-02-04 13:17:14 -0800

    Add event listener on join game

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
   App.game = App.cable.subscriptions.create("GameChannel", {
     connected: function() {
-      // Called when the subscription is ready for use on the server      
-      this.perform('speak', { message: 'Hello world' })
+      // Called when the subscription is ready for use on the server
+      // this.perform('speak', { message: 'Hello world' })
     },
 
     disconnected: function() {
@@ -11,14 +11,27 @@ $( document ).ready(function() {
     },
 
     received: function(message) {
-        // Called when there's incoming data on the websocket for this channel
-        alert(message)
+      // Called when there's incoming data on the websocket for this channel
+      alert(message['message'])
     },
 
     speak: function(message) {
       this.perform('speak', { message: message })
     }
   });
+
+  $('#join-game-form').on('submit', function(event) {
+    event.preventDefault()
+    alert('Listening on join game')
+    $.ajax({
+      url: '/games/join',
+      method: 'POST'
+      // data: { channel: App.game }
+      // success: function(result) {
+      //   alert(result)
+      // }
+    })
+  })
 });
 
 
